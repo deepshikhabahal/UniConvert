fabb2dc26fbe87566abac8ec3a95314a2a590c03
Deepshikha Bahal <deepshikhabahal@gmail.com>
2021-05-13 14:44:03 +0530

    converting kg values into pounds

---
+++ app/src/main/java/com/example/uniconvert/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.uniconvert;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private TextView textView;
+    private EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+        editText = findViewById(R.id.editText);
+    }
+    public void calculate(View view){
+        String s = editText.getText().toString();
+        int kg = Integer.parseInt(s);
+        double pound = 2.205 * kg;
+        textView.setText("The corresponding value in Pounds is  " + pound);
+    }
+    }
